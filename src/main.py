5a02948089ba52dab3aa9573bf3d059a6ae40b35
zapagenrevdale <zapagenrevdale@gmail.com>
2023-08-27 21:50:38 +0800

    Add user routes in main module

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from src.users.router import router as user_router
 from src.auth.router import router as auth_router
+
+from src.database import engine
+from src.models import Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+app.include_router(user_router, prefix="/users", tags=["User"])
+app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
